7fb2c6f5fcf17990252b5d623cf29cb603371461
alan <vagaab@foxmail.com>
2021-05-20 22:23:08 +0800

    Update main 支持多线程运行

--- main.py
+++ main.py
@@ -5,14 +5,23 @@
 import click
 
 from WebSpider.spider import Spider
+from WebSpider.threads import MultiThread
+
+
+def create_spider(use_plugin, use_proxy):
+    spider = Spider(use_plugin=use_plugin, use_proxy=use_proxy)
+    spider.run()
 
 
 @click.command()
 @click.option('--use_plugin', type=bool, default=False, help="是否加载插件")
 @click.option('--use_proxy', type=bool, default=False, help="是否使用代理")
-def run(use_plugin, use_proxy):
-    spider = Spider(use_plugin=use_plugin, use_proxy=use_proxy)
-    spider.run()
+@click.option('--workers', type=int, default=None, help="使用线程数量，默认为CPU内核数量的5倍")
+@click.option('--times', type=int, default=0, help="一共访问多少次，默认10次")
+def run(use_plugin, use_proxy, workers, times):
+    pool = MultiThread(max_workers=workers)
+    for _ in range(times * 100 or 10):
+        pool.execute(create_spider, use_plugin, use_proxy)
 
 
 # Press the green button in the gutter to run the script.
