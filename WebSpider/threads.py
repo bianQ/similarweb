1e89118535baa4703c7acfb06ee24a6b9f775e46
alan <vagaab@foxmail.com>
2021-05-23 14:15:03 +0800

    Update threads
    - 删除 self.workers
    - Fix execute_after_done 必须所有任务执行完才能调用 fn

--- WebSpider/threads.py
+++ WebSpider/threads.py
@@ -12,14 +12,11 @@ class MultiThread(ThreadPoolExecutor):
 
     def __init__(self, max_workers=None, thread_name_prefix=''):
         super().__init__(max_workers, thread_name_prefix)
-        self.workers = []
 
     def thread_log(self, worker):
         """捕获线程异常，并保存日志"""
         try:
             result = worker.result()
-            if worker in self.workers:
-                self.workers.remove(worker)
             return result
         except:
             traceback.print_exc()
@@ -28,10 +25,9 @@ class MultiThread(ThreadPoolExecutor):
         """生成新线程，并捕捉异常"""
         thread = self.submit(fn, *args, **kwargs)
         thread.add_done_callback(self.thread_log)
-        self.workers.append(thread)
         return thread
 
-    def execute_after_done(self, fn, *args, workers=None, **kwargs):
-        workers = workers or self.workers
+    @staticmethod
+    def execute_after_done(fn, workers, *args, **kwargs):
         wait(workers, timeout=86400, return_when=ALL_COMPLETED)
-        self.execute(fn, *args, **kwargs)
+        return fn(*args, **kwargs)
