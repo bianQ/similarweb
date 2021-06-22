bc09a30adc166644fa8838db94f5c21b98ec0632
bianQ <vagaab@foxmail.com>
2021-06-22 13:49:05 +0800

    Update 日志

--- WebSpider/spider.py
+++ WebSpider/spider.py
@@ -216,7 +216,7 @@ class Spider:
         if len(self.dr.window_handles) > 1:
             self.dr.switch_to.window(self.dr.window_handles[0])
         if 'kline' in self.dr.current_url:
-            self.logger.info(f"{self}：open kline")
+            self.logger.info(f"{self}：open kline {self.dr.current_url}")
         self.click_nav()
         parser = urlparse(self.dr.current_url)
         path = parser.path.replace(self.main_path, '').split('/')[0]
