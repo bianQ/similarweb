8a70f54723a8f8a98780cc0742972f127b93ad9b
Alan <vagaab@foxmail.com>
2021-07-25 12:26:15 +0800

    add test

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+"""
+Author   : Alan
+Date     : 2021/7/25 12:01
+Email    : vagaab@foxmail.com
+"""
---
+++ test/proxy_test.py
@@ -0,0 +1,25 @@
+"""
+Author   : Alan
+Date     : 2021/7/25 12:01
+Email    : vagaab@foxmail.com
+"""
+import requests
+
+from WebSpider.proxy import proxy_server
+
+
+def main():
+    session = requests.session()
+    url = 'http://myip.ipip.net'
+    print('========== 本机ip ==========')
+    resp1 = session.get(url)
+    print(resp1.text)
+    print('========== 代理ip ==========')
+    proxy = proxy_server.get_proxy()
+    print(f"代理配置 {proxy}")
+    resp2 = session.get(url, proxies={'https': f"https://{proxy['ip']}:{proxy['port']}"})
+    print(resp2.text)
+
+
+if __name__ == '__main__':
+    main()
